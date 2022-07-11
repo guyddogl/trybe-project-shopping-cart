ace8fcfefd25cf7cc8da4d7b5dbb368f58212842
Guyddo <guyddogl@gmail.com>
2022-07-11 15:07:42 -0300

    Verifica se fetch foi chamada ao executar a função fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,9 @@ describe('1 - Teste a função fetchProducts', () => {
   test('Verifica se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
+  test('Verifica se fetch foi chamada ao executar a função fetchProducts com o parâmetro computador', async () => {
+    expect.assertions(1);
+    await fetchProducts("computador");
+    expect(fetch).toHaveBeenCalled();
+  });
 });
