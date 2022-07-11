591e5013ab071b8a72436694da4b6561e79c861a
Guyddo <guyddogl@gmail.com>
2022-07-11 16:54:19 -0300

    Requisito 3 - função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  if (!item) throw new Error('You must provide an url');
+  try {
+    const URL = `https://api.mercadolibre.com/items/${item}`;
+    const result = await fetch(URL);
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return `Erro: ${error}`;
+  }
 };
 
 if (typeof module !== 'undefined') {
