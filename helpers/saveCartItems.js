896efe7cb446038956792dce4e345bf1926dc217
Guyddo <guyddogl@gmail.com>
2022-07-11 19:32:59 -0300

    Cria função saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,6 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (item) => {
+  const saveCartList = document.getElementById('list-cart').innerHTML;
+  localStorage.setItem('cartItems', saveCartList);
 };
 
 if (typeof module !== 'undefined') {
