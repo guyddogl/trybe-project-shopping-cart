d87bff7050a6ab052311f3b32b7e51af4501c096
Guyddo <guyddogl@gmail.com>
2022-07-11 20:10:50 -0300

    Salva o preço total do carrinho no local storage

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,8 @@
 const saveCartItems = () => {
   const saveCartList = document.getElementById('list-cart').innerHTML;
   localStorage.setItem('cartItems', saveCartList);
+  const saveTotalPrice = totalPrice.innerText;
+  localStorage.setItem('totalPrice', saveTotalPrice);
 };
 
 if (typeof module !== 'undefined') {
