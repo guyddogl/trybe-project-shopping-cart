35064047bb51edc760111b7025ad4dca682c3d81
Guyddo <guyddogl@gmail.com>
2022-07-11 19:44:43 -0300

    Correções de lint

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,4 +1,4 @@
-const saveCartItems = (item) => {
+const saveCartItems = () => {
   const saveCartList = document.getElementById('list-cart').innerHTML;
   localStorage.setItem('cartItems', saveCartList);
 };
