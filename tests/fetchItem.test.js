781f4ca700dc0b934e1cc97acf05f6e9939e941f
Guyddo <guyddogl@gmail.com>
2022-07-11 16:52:56 -0300

    Verifica se fetchItem utilizou a url https://api.mercadolibre.com/items/MLB1615760527

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -11,4 +11,10 @@ describe('2 - Teste a função fetchItem', () => {
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled();
   });
+  test('Verifica se fetchItem utilizou a url https://api.mercadolibre.com/items/MLB1615760527', async () => {
+    expect.assertions(1);
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith(url);
+  });
 });
