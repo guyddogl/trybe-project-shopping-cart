80260a9b7ad4c013dae31dfe6210024fbc7fa6a6
Guyddo <guyddogl@gmail.com>
2022-07-11 15:14:34 -0300

    Requisito 1 - fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,17 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  if (!product) throw new Error('You must provide an url');
+  try {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const result = await fetch(URL);
+    const data = await result.json();
+    // console.log(data.results);
+    return data;
+  } catch (error) {
+    return console.log(`Deu erro: ${error}`);
+  }
 };
+
+// fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
