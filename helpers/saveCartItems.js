090e98112fcd72a614a267d52c06f9697e9f46d5
Guyddo <guyddogl@gmail.com>
2022-07-12 09:33:44 -0300

    Refatorando saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,8 +1,5 @@
-const saveCartItems = () => {
-  const saveCartList = document.getElementById('list-cart').innerHTML;
-  // const saveTotalPrice = document.getElementById('total-price').innerText;
-  return localStorage.setItem('cartItems', saveCartList);
-  // localStorage.setItem('totalPrice', saveTotalPrice);
+const saveCartItems = (saveCartList) => {
+  localStorage.setItem('cartItems', saveCartList);
 };
 
 if (typeof module !== 'undefined') {
